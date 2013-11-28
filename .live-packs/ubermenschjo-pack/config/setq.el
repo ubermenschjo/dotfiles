e2e7ce8ea06cc536d1c352a5068984fc085bc3e1
Dell laptop <jjl@invenzion.co.kr>
2013-11-29 05:53:14 +0900

    emacs 설정파일 추가

---
+++ .live-packs/ubermenschjo-pack/config/setq.el
@@ -0,0 +1,12 @@
+(setq completion-ignore-case t           ;; ignore case when completing...
+      read-file-name-completion-ignore-case t) ;; ...filenames too
+;; backups
+(setq make-backup-files t ;; do make backups
+      backup-by-copying t     ;; and copy them here
+      backup-directory-alist '(("." . "~/.emacs.d/cache/backups"))
+      version-control t
+      kept-new-versions 2
+      kept-old-versions 5
+      delete-old-versions t)
+
+(setq inferior-lisp-program "/usr/bin/sbcl")
---
+++ .live-packs/ubermenschjo-pack/init.el
@@ -0,0 +1,14 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+(load (expand-file-name "~/quicklisp/slime-helper.el") t t t)
+(when (require 'slime nil 'noerror)
+  (slime-setup))
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(live-load-config-file "elpa.el")
+(live-load-config-file "setq.el")
+(live-load-config-file "dotshare.el")
