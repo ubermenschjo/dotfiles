4ed396d6348eb9be4d1de9d492aedf7573b81a8e
Dell laptop <jjl@invenzion.co.kr>
2013-12-07 08:22:31 +0900

    add binding config

---
+++ .live-packs/ubermenschjo-pack/config/bindings.el
@@ -0,0 +1,7 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+(define-key global-map (kbd "C-x b") 'anything)
+(define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
