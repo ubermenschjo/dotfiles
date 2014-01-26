1b6802227b71aaf8a643ffc870ca082c01149cf2
Dell laptop <jjl@invenzion.co.kr>
2014-01-26 20:45:18 +0900

    buffer navigation : helm-buffers

--- .live-packs/ubermenschjo-pack/config/bindings.el
+++ .live-packs/ubermenschjo-pack/config/bindings.el
@@ -1,7 +1,7 @@
 ;; Place your bindings here.
 
 ;; For example:
-;;(define-key global-map (kbd "C-+") 'text-scale-increase)
-;;(define-key global-map (kbd "C--") 'text-scale-decrease)
-(define-key global-map (kbd "C-x b") 'anything)
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
 (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
