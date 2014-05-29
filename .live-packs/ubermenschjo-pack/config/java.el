d251d3bcfc806c182fa2b5b7a5a6cc4b4cd51f4b
x201 laptop <jjl@vopier.co.kr>
2014-05-29 22:53:35 +0900

    eclimd settings merge

--- .live-packs/ubermenschjo-pack/config/java.el
+++ .live-packs/ubermenschjo-pack/config/java.el
@@ -1,8 +1,28 @@
-(require 'cedet)
-(require 'semantic)
+;;; Malabar-mode
+;; (require 'cedet)
+;; (require 'semantic)
 
-(load "semantic/loaddefs.el")
-(semantic-mode 1)
+;; (load "semantic/loaddefs.el")
+;; (semantic-mode 1)
 
-(require 'malabar-mode)
-(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+;; (require 'malabar-mode)
+;; (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+;;; eclim
+(require 'eclim)
+(global-eclim-mode)
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'ac-emacs-eclim-source)
+;;; (ac-emacs-eclim-source)
+
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+(require 'company)
+(require 'company-emacs-eclim)
+(company-emacs-eclim-setup)
+(global-company-mode t)
